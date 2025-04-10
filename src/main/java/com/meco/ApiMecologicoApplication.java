fe50569384529d2f956d57204404ca930ed50c17
dmarlop <davidmarinlpz@gmail.com>
2025-04-10 15:25:33 +0200

    Proyecto creado de 0, spring inicializado, nada mas

---
+++ src/main/java/com/meco/ApiMecologicoApplication.java
@@ -0,0 +1,13 @@
+package com.meco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMecologicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMecologicoApplication.class, args);
+	}
+
+}
